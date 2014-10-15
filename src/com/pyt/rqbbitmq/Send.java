d18a3725c437f69583d3a2b7419a7933435dbd7c
haley <95yute@163.com>
2014-10-15 17:31:30 +0800

    first commit

---
+++ src/com/pyt/rqbbitmq/Send.java
@@ -0,0 +1,26 @@
+package com.pyt.rqbbitmq;
+
+import java.io.IOException;  
+  
+import com.rabbitmq.client.Channel;  
+import com.rabbitmq.client.Connection;  
+import com.rabbitmq.client.ConnectionFactory;  
+  
+public class Send {  
+    private final static String QUEUE_NAME = "hello1";  
+  
+    public static void main(String[] args) throws IOException {  
+        ConnectionFactory factory = new ConnectionFactory();  
+        factory.setHost("localhost");  
+        Connection connection = factory.newConnection();  
+        Channel channel = connection.createChannel();  
+  
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);  
+        String message = "Hello World11!";  
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());  
+        System.out.println(" [x] Sent '" + message + "'");  
+  
+        channel.close();  
+        connection.close();  
+    }  
+}  
